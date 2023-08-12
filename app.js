ec40f410e1b0da0d4b71bbbab1dfc870d36cd25a
Niraj6295 <137032658+Niraj6295@users.noreply.github.com>
2023-08-12 17:42:45 +0530

    added

---
+++ app.js
@@ -0,0 +1,30 @@
+let screen = document.querySelector(".screen");
+screen.innerText=0
+let string="";
+
+let buttons = document.querySelectorAll(".key");
+buttons.forEach(element => {
+    element.addEventListener("click",(e)=>{
+        document.querySelector(".normal-sound").play()
+        console.log(e.target);
+        try{
+            if(e.target.innerText=="="){
+                string= eval(string);
+                screen.innerText = string;
+            }else if(e.target.innerText=="AC"){
+                string="";
+                screen.innerText = string;
+            }else if(e.target.innerText=="C"){
+                string = string.slice(0,(string.length-1));
+                screen.innerText = string;
+            }
+            else{
+                string= string+ e.target.innerHTML;
+                console.log(string);
+                screen.innerText = string;
+            };
+        }catch{
+            screen.innerText = "INVALID-INPUT";
+        }
+    });
+});
